9420854a68419fd98e3bdf5b6133e2af62d8a1a9
defm03 <yutsuro@gmail.com>
2013-05-14 18:47:42 +0200

    i've made some decision i guess import base64,os,sys,cmd

--- src/ayanami-file.py
+++ src/ayanami-file.py
@@ -1,23 +1,15 @@
 import base64
 import os
 import sys
+import cmd
 
-print_info = False
-type_in = ''
-
-def type_in():
-  type_in = raw_input('> ')
-
-def print_info(type_in):
-  if type_in == "print info":
-    print_info = True
-  if print_info == True:
+class cmd_input(cmd.Cmd):
+  def print_info(self,line):
     print os.name 
     print sys.platform
     print os.uname()
+  def quit(self,line):
+    sys.exit("sayonara")
 
-if type_in == "quit":
-  sys.exit("sayonara~")
-
-while not type_in == "quit":
-  type_in()
+if __name__ == '__main__':
+  cmd_input().cmdloop()
