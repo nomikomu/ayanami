9b5dfd6bfbaed793762bddc4c2cb1996d097dca5
defm03 <yutsuro@gmail.com>
2013-05-14 19:24:37 +0200

    time? oh mai gah, hao

--- src/ayanami-file.py
+++ src/ayanami-file.py
@@ -1,7 +1,10 @@
+#-*- coding: utf-8 -*-
+
 import base64
 import os, os.path
 import sys
 import cmd
+import time
 
 # This program is free software; you can redistribute it and #
 # /or modify it under the terms of the GNU General  Public - #
@@ -19,10 +22,11 @@ import cmd
 #                                                            #
 #   NERV HQ - defm03 // github@defm03 // yutsuro@gmail.com   #
 
+localtime = time.localtime(time.time())
 class cmd_input(cmd.Cmd):
-  def do_print_info(self,line):
-    print os.name 
-    print sys.platform
+  prompt = str(localtime) + ' λ.春: '
+  def do_sysinfo(self,line):
+    print '[ ' + os.name + ' ' + sys.platform + ' ]'
     print os.uname()
   def do_count_files(self,line):
     print len([name for name in os.listdir('.') if os.path.isfile(name)])
