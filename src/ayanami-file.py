9c0623945c85da1ae860d1c03d995faf0b9b61ae
defm03 <yutsuro@gmail.com>
2013-05-14 18:25:11 +0200

    whas goin on

--- src/ayanami-file.py
+++ src/ayanami-file.py
@@ -1,6 +1,23 @@
 import base64
 import os
 import sys
-print os.name 
-print sys.platform
-print os.uname()
+
+print_info = False
+type_in = ''
+
+def type_in():
+  type_in = raw_input('> ')
+
+def print_info(type_in):
+  if type_in == "print info":
+    print_info = True
+  if print_info == True:
+    print os.name 
+    print sys.platform
+    print os.uname()
+
+if type_in == "quit":
+  sys.exit("sayonara~")
+
+while not type_in == "quit":
+  type_in()
