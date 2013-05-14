329f37d0f11e66b0ebf08eea93ebb87a06e62ea8
defm03 <yutsuro@gmail.com>
2013-05-14 18:58:25 +0200

    cmd.Cmd class - i don't like that 'do_'

--- src/ayanami-file.py
+++ src/ayanami-file.py
@@ -3,12 +3,28 @@ import os
 import sys
 import cmd
 
+# This program is free software; you can redistribute it and #
+# /or modify it under the terms of the GNU General  Public - #
+# License as published by the Free Software Foundation; eit- #
+# her version 2 of the License, or (at your option) any lat- #
+# er version.                          Copyright 2013 defm03 #
+#                                                            #
+#              /     \                                       #
+#              vvvvvvv  /|__/|                               #
+#                 I   /O,O   |                               #
+#                 I /_____   |      /|/|                     #
+#                J|/^ ^ ^ \  |    /00  |    _//|             #
+#                 |^ ^ ^ ^ |W|   |/^^\ |   /oo |             #
+#                  \m___m__|_|    \m_m_|   \mm_|             #
+#                                                            #
+#   NERV HQ - defm03 // github@defm03 // yutsuro@gmail.com   #
+
 class cmd_input(cmd.Cmd):
-  def print_info(self,line):
+  def do_print_info(self,line):
     print os.name 
     print sys.platform
     print os.uname()
-  def quit(self,line):
+  def do_quit(self,line):
     sys.exit("sayonara")
 
 if __name__ == '__main__':
